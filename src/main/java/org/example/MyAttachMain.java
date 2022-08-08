0ab937b8c5a7c34cbb73b63a8fea5889108b9d73
zhou peng <peng.zhou15@lotuscars.com.cn>
2022-08-08 18:22:12 +0800

    初始化

--- src/main/java/org/example/MyAttachMain.java
+++ src/main/java/org/example/MyAttachMain.java
@@ -6,8 +6,10 @@ public class MyAttachMain {
     public static void main(String[] args) {
         VirtualMachine vm = null;
         try {
-            vm = VirtualMachine.attach("76293");//MyBizMain进程ID
-            vm.loadAgent("/Users/zhp/Documents/AgentDemo/target/agent-demo-jar-with-dependencies.jar");//java agent jar包路径
+            //进程ID
+            vm = VirtualMachine.attach("76293");
+            //java agent jar包路径
+            vm.loadAgent("/Users/zhp/Documents/AgentDemo/target/agent-demo-jar-with-dependencies.jar");
         } catch (Exception e) {
             e.printStackTrace();
         }
